1dc2f6618544cd9caec44d47ff5c7ccbc7ed6b89
Leah <leah_nelson2@baylor.edu>
2024-02-08 14:48:00 -0600

    hide files

--- exception example 1.py
+++
@@ -1,9 +0,0 @@
-#answer = float(input("How many hours did you work? "))
-
-try:
-    answer = float(input("How many hours did you work? "))
-except:
-    pass
-    #print("there was an error")
-
-print('Done!')
--- read_clients.py
+++
@@ -1,9 +0,0 @@
-def main():
-    infile = open('clients.txt', 'r')
-
-    i = 1
-    for line in infile:
-        print(i, '. ', line, end='', sep='')
-        i += 1
-
-main()
